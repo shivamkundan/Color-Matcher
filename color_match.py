df6ed1699eac0a03610ef732968d0e2c0f99730d
Shivam Kundan <shivamkundan@hotmail.com>
2023-05-18 08:48:24 -0500

    formatting

--- color_match.py
+++ color_match.py
@@ -9,7 +9,7 @@ pygame.init()
 
 screen_w=440
 screen_h=200
-screen = pygame.display.set_mode((screen_w, screen_h),pygame.HWSURFACE | pygame.DOUBLEBUF)
+screen = pygame.display.set_mode((screen_w, screen_h),pygame.RESIZABLE | pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.ASYNCBLIT)
 pygame.display.set_caption('Color Matcher')
 black = pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
@@ -19,14 +19,27 @@ HELVETICA=pygame.freetype.Font('HelveticaNeue.ttc')
 
 clock = pygame.time.Clock()
 # ==========================================================================
-# 0xFFFFFF = 16,777,215
+rand_colors=[]
+
+for i in range(300):
+	new=(random.randrange(0, 255+1),random.randrange(0, 255+1),random.randrange(0, 255+1))
+	# print (f"new:{new}")
+	rand_colors.append(new)
+
+in_color_list=[(0,72,186),(0,0,0),(132,17,12),(213,203,77),\
+				(128,0,0),(64,0,0),(173,234,234),(0,120,0),(100,255,255),\
+				(70,130,109),(0,0,64),(255,128,0),\
+				(100,90,100),(122,37,29),(122,0,29),(240,230,140)]
+
+in_color_list=rand_colors+in_color_list
+FPS=5
+
+# ==========================================================================
 def rgb_to_int(in_color):
 	return (int('%02x%02x%02x' % in_color,16))
 
 def hex_to_rgb(raw):
 	print (f"raw:{raw}")
-	l=len(raw)
-	print (f"len(raw):{l}")
 
 	# If R=0
 	if l==4:
@@ -97,19 +110,23 @@ def color_match_by_rgb(input_color,df):
 		B=df["B"][index]
 		name=df["Name"][index]
 
+
+
 		# Calculate
 		diff_R=abs(R-input_color[0])
 		diff_G=abs(G-input_color[1])
 		diff_B=abs(B-input_color[2])
+
 		diff_avg=round((diff_R+diff_B+diff_G)/3,2)
 
 		# Compare
 		if diff_avg<=min_diff:
 			min_diff=diff_avg
-			min_code=(R,G,B)
+			closest_match=(R,G,B)
 			min_diff_rgb=(diff_R,diff_G,diff_B)
 			min_name=name
 
+			# print (name,diff_avg)
 			# print (f"{index}: {name} {min_code} {min_diff}\n[{min_diff_rgb}]\n")
 
 	# # print ()
@@ -118,7 +135,7 @@ def color_match_by_rgb(input_color,df):
 	# print (f"               {min_name}: {min_code} ")
 	# print (f"               diff: {min_diff} [{min_diff_rgb}]")
 
-	return min_code,min_diff_rgb,min_diff,min_name
+	return closest_match, min_diff_rgb, min_diff, min_name
 
 # ==========================================================================
 
@@ -130,20 +147,23 @@ def blit_current_color(in_color):
 def blit_closest_color(in_color):
 	pygame.draw.rect(screen, in_color, (COL,ROW+50, 100, 30))
 
-rand_colors=[]
+def event_handler(color_num):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key==113)):
+			pygame.quit()
+			sys.exit()
+		elif (event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT)):
+			if color_num==len(in_color_list)-1:
+				color_num=0
+			else:
+				color_num+=1
+		elif (event.type == pygame.KEYDOWN and (event.key == pygame.K_LEFT)):
+			if color_num==0:
+				color_num=len(in_color_list)-1
+			else:
+				color_num-=1
+	return color_num
 
-for i in range(100):
-	new=(random.randrange(0, 255+1),random.randrange(0, 255+1),random.randrange(0, 255+1))
-	# print (f"new:{new}")
-	rand_colors.append(new)
-
-in_color_list=[(0,72,186),(0,0,0),(132,17,12),(213,203,77),\
-(128,0,0),(64,0,0),(173,234,234),(0,120,0),(100,255,255),\
-(70,130,109),(0,0,64),(255,128,0),\
-(100,90,100),(122,37,29),(122,0,29),(240,230,140)
-]
-
-in_color_list=rand_colors+in_color_list
 
 if __name__ == "__main__":
 
@@ -155,23 +175,10 @@ if __name__ == "__main__":
 
 		in_color_code=in_color_list[color_num]
 
-		clock.tick(5)
-		# We just look to see if the user wants to exit.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key==113)):
-				done = True
-				pygame.quit()
-				sys.exit()
-			elif (event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT)):
-				if color_num==len(in_color_list)-1:
-					color_num=0
-				else:
-					color_num+=1
-			elif (event.type == pygame.KEYDOWN and (event.key == pygame.K_LEFT)):
-				if color_num==0:
-					color_num=len(in_color_list)-1
-				else:
-					color_num-=1
+		clock.tick(FPS)
+
+
+		color_num=event_handler(color_num)
 
 
 		screen.fill(black)
@@ -180,15 +187,14 @@ if __name__ == "__main__":
 		blit_current_color(in_color_code)
 
 		# Find closest match
-		min_code,min_diff_rgb,min_diff,min_name=color_match_by_rgb(in_color_code,df)
+		closest_match,min_diff_rgb,min_diff,min_name=color_match_by_rgb(in_color_code,df)
 
-		# HELVETICA.render_to(screen, (30,80), f"closest:", white,style=0,size=18)
 		HELVETICA.render_to(screen, (30,100), f"{min_name}", white,style=0,size=18)
-		HELVETICA.render_to(screen, (30,120), f"{min_code}", white,style=0,size=18)
+		HELVETICA.render_to(screen, (30,120), f"{closest_match}", white,style=0,size=18)
 		HELVETICA.render_to(screen, (30,140), f"diff: {min_diff_rgb}", white,style=0,size=16)
 		HELVETICA.render_to(screen, (30,160), f"diff avg: {min_diff}", white,style=0,size=16)
 
-		blit_closest_color(min_code)
+		blit_closest_color(closest_match)
 
 
 		pygame.display.update()
